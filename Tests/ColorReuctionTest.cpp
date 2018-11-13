f403dd941f0f2e5db2bc6a1f1cea9bf9e6dd9329
XiangDe <qq313700046@icloud.com>
2018-11-14 00:05:00 +0800

    the conversion test between vec3f and Matrix.Reshape will make the row,col and dims change,but remain row * col * dim equal before.It's O(1) operation.

--- Tests/ColorReuctionTest.cpp
+++ Tests/ColorReuctionTest.cpp
@@ -36,6 +36,7 @@ TEST(COLOR_REDUCTION_TEST,test_reduce_color_16_million_by_divideWidth){
     cv::Mat img = cv::imread("rgb/1.jpg");
     std::cout << "Image Size is (" << img.rows << ","<< img.cols << ")" << std::endl;
     std::cout << "Image Channels:" << img.channels() << std::endl;
+    std::cout << "Image Depth :" << img.depth() << std::endl;
     uchar *table = new uchar[256];
     if (img.data == NULL) {
         std::cout << "Empty Image." << std::endl;
@@ -46,4 +47,18 @@ TEST(COLOR_REDUCTION_TEST,test_reduce_color_16_million_by_divideWidth){
     cv::Mat afterReductionImg = scanImgAndReduceImgColor(img, table);
     cv::imshow("AfterReductionImage", afterReductionImg);
     cv::waitKey(0);
+}
+
+TEST(POINT_TRANSMITION_TEST,test_point3f_cvt_MAT){
+    std::vector<cv::Vec3f> points;
+    points.emplace_back(1, 1, 1);
+    points.emplace_back(2, 2, 2);
+    points.emplace_back(3, 3, 3);
+    cv::Mat mat(points);
+   std::cout<< "Matrix channels is:" << mat.channels() << std::endl;
+   ASSERT_TRUE( mat.channels() == 3);
+    cv::Mat pointMat = cv::Mat(points).reshape(1);
+    //std::cout << "Reshaped Mat:" << pointMat << std::endl;
+    std::cout << "Reshaped Matrix channels is:" << pointMat.channels() << std::endl;
+    ASSERT_TRUE(pointMat.channels() == 1);
 }
